3a1e32e7c471bd1a03f56fba6e27dffe1bfe1039
ChemistrywithShane <ratcliffeshane@hotmail.com>
2025-10-01 11:06:24 +0100

    Update presets.ts

--- chemistry-with-shane-titration-lab/src/data/presets.ts
+++ chemistry-with-shane-titration-lab/src/data/presets.ts
@@ -1,1 +1,25 @@
+export interface Preset {
+  name: string;
+  analyteConcentration: number; // mol/L
+  titrantConcentration: number;
+  analyteVolume: number; // in L
+  titrantVolumeMax: number; // in L
+}
+
+export const presets: Preset[] = [
+  {
+    name: "HCl + NaOH (0.1 M)",
+    analyteConcentration: 0.1,
+    titrantConcentration: 0.1,
+    analyteVolume: 0.025,
+    titrantVolumeMax: 0.025,
+  },
+  {
+    name: "HCl + NaOH (0.05 M)",
+    analyteConcentration: 0.05,
+    titrantConcentration: 0.05,
+    analyteVolume: 0.025,
+    titrantVolumeMax: 0.05,
+  },
+];
 
